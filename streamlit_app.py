0dcb1bfea31cbc156175264e92f23a5d4b7eb0a1
SnowFlakeTitusSys <113317205+SnowFlakeTitusSys@users.noreply.github.com>
2022-09-18 18:17:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-#streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
 
 #streamlit.header('Fruityvice Fruit Advice!')
 
